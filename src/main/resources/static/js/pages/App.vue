18331edc7d7fdc357274c9854fa9e533eaaedc0e
Elvolver <elvolver@mail.ru>
2020-05-29 02:31:30 +0300

    Add Vuex Add Router

--- src/main/resources/static/js/pages/App.vue
+++ src/main/resources/static/js/pages/App.vue
@@ -1,54 +1,62 @@
 <template>
     <v-app>
         <v-app-bar app>
-            <v-toolbar-title>Социальная сеть ёпта</v-toolbar-title>
+            <v-toolbar-title>Асоциальная сеть ёпта</v-toolbar-title>
+            <v-btn v-if="profile" text :disabled="$route.path === '/'" @click="showMessages">
+                Messages
+            </v-btn>
             <v-spacer></v-spacer>
-            <span v-if="profile">{{profile.name}}</span>
+            <v-btn v-if="profile" text :disabled="$route.path === '/profile'" @click="showProfile">
+                {{profile.name}}
+            </v-btn>
             <v-btn v-if="profile" icon href="/logout">
                 <v-icon>mdi-exit-to-app</v-icon>
             </v-btn>
         </v-app-bar>
         <v-content>
-            <v-container v-if="!profile">
-                Необходимо авторизоваться через <a href="/login">Google</a>
-            </v-container>
-            <v-container v-if="profile">
-                <messages-list :messages="messages"/>
-            </v-container>
+
+            <router-view>
+
+            </router-view>
         </v-content>
     </v-app>
 </template>
 
 <script>
-    import MessagesList from "components/messages/MessagesList.vue";
+    import {mapState, mapMutations} from 'vuex'
     import {addHandler} from "util/ws";
+    import router from "../router/router";
 
     export default {
-        components: {
-            MessagesList
-        },
-        data() {
-            return {
-                messages: frontendData.messages,
-                profile: frontendData.profile
+        components: {},
+        computed: mapState([
+            'profile'
+        ]),
+        methods: {
+            ...mapMutations([
+                'addMessageMutation',
+                'updateMessageMutation',
+                'removeMessageMutation'
+            ]),
+            showMessages() {
+                this.$router.push('/')
+            },
+            showProfile() {
+                this.$router.push('/profile')
             }
         },
         created() {
             addHandler(data => {
                 if (data.objectType === 'MESSAGE') {
-                    const index = this.messages.findIndex(item => item.id === data.body.id);
-
                     switch (data.eventType) {
                         case 'CREATE':
+                            this.addMessageMutation(data.body)
+                            break
                         case 'UPDATE':
-                            if (index > -1) {
-                                this.messages.splice(index, 1 ,data.body)
-                            } else {
-                                this.messages.push(data.body)
-                            }
+                            this.updateMessageMutation(data.body)
                             break
                         case 'REMOVE':
-                            this.messages.splice(index, 1)
+                            this.removeMessageMutation(data.body)
                             break
                         default:
                             console.error(`Looks like the event type if unknown "${data.eventType}"`)
@@ -57,6 +65,11 @@
                     console.error(`Looks like the object type if unknown "${data.objectType}"`)
                 }
             })
+        },
+        beforeMount() {
+            if (!this.profile) {
+                this.$router.replace('/auth')
+            }
         }
     }
 </script>
---
+++ src/main/resources/static/js/pages/Auth.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-container v-if="!profile">
+        Необходимо авторизоваться через <a href="/login">Google</a>
+    </v-container>
+</template>
+<script>
+    export default {
+        name: 'Auth'
+    }
+</script>
+<style>
+
+</style>
